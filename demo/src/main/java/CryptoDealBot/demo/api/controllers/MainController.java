cbe9977a500942cf618b4e2cf2188497ae37cc14
Rodion <rodionchik17@gmail.com>
2024-05-18 15:13:25 +0300

    updated controller class

--- demo/src/main/java/CryptoDealBot/demo/api/controllers/MainController.java
+++ demo/src/main/java/CryptoDealBot/demo/api/controllers/MainController.java
@@ -1,23 +1,17 @@
 package CryptoDealBot.demo.api.controllers;
 
-import CryptoDealBot.demo.business.exceptions.TemporaryDataUnavailableException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+
+import static CryptoDealBot.demo.business.RequestSender.sendNextRequest;
+import static CryptoDealBot.demo.business.ResponseJsonParser.getMarkPrice;
 
 @Tag(name = "CryptoPair controller")
 @RestController
@@ -61,23 +55,5 @@ public class MainController {
         } catch (IOException e) {
             return e.getMessage();
         }
-    }
-
-    @Retryable(value = {TemporaryDataUnavailableException.class}, maxAttempts = 3, backoff = @Backoff(delay = 2000))
-    private static String sendNextRequest(String URL) {
-        try {
-            String response = new RestTemplate().getForEntity(URL, String.class).getBody();
-            if (response == null || response.isEmpty())
-                throw new TemporaryDataUnavailableException("Data is not available yet. Please try again.");
-            return response;
-        } catch (HttpClientErrorException | ResourceAccessException e) {
-            return e.getMessage();
-        }
-    }
-
-    private static double getMarkPrice(String jsonString) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode = objectMapper.readTree(jsonString);
-        return rootNode.get("markPrice").asDouble();
     }
 }
